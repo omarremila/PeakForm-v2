94b649cae736c203a6b2fbd65297ee42314b6adf
Omar G <omarg@Omars-Mac.local>
2026-08-19 12:37:10 -0400

    Initial PeakForm setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+// src/pages/Home.jsx
+import React from 'react';
+
+function Home() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h2>Home Page</h2>
+      <p>Welcome to our App’s home page!</p>
+    </div>
+  );
+}
+
+export default Home;
