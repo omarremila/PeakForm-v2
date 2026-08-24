19f23139b73d309dfb00147716acb1ee62704bbd
Omar G <omarg@omars-mac.home>
2026-08-24 13:23:07 -0400

    Update: new logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,22 +5,27 @@ import './Header.css'
 
 export default function Header() {
   return (
-    <header className="header">
-      <div className="header-logo">
-      <h1 className="header-text">LockedIn</h1>
+    
+   <header className="header">
+  <Link to="/" className="header-logo">
+    <img
+      src={logo}
+      alt=""
+      aria-hidden="true"
+      className="muscle-icon muscle-icon-mirrored"
+    />
+    <h1 className="header-text">LockedIn</h1>
+    <img
+      src={logo}
+      alt="LockedIn muscle icon"
+      className="muscle-icon"
+    />
+  </Link>
 
-        <img
-          src={logo}
-          alt="PeakForm muscle icon"
-          className="muscle-icon"
-        />
-      </div>
-
-      <nav className="nav nav-right">
-        <Link to="/">Home</Link>
-        <Link to="/choose-workout">Choose Workout</Link>
-        <Link to="/signup">Sign Up</Link>
-      </nav>
-    </header>
+  <nav className="nav nav-right">
+    <Link to="/">Home</Link>
+    <Link to="/choose-workout">Choose Workout</Link>
+  </nav>
+</header>
   )
 }
