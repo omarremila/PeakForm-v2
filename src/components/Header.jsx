8c75546862e052e66dcbf17baf8648c46ef2b2a8
Omar G <omarg@omars-mac.home>
2026-08-24 12:58:24 -0400

    New Name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
   return (
     <header className="header">
       <div className="header-logo">
-      <h1 className="header-text">PeakForm Coach</h1>
+      <h1 className="header-text">LockedIn</h1>
 
         <img
           src={logo}
